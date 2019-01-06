51005adb96fdf7f4f24663c11374135a03d81cea
Amish <amir.shabanai68@gmail.com>
2019-01-06 17:08:51 +0330

    restart project

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-import { AppRegistry } from 'react-native';
-import App from './src/app/App';
+/** @format */
 
-AppRegistry.registerComponent('trade-core-game', () => App);
+import {AppRegistry} from 'react-native';
+import App from './src/app/App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
