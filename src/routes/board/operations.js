0090b7c2aaac808051758094091d0614af006fd9
Amish <amir.shabanai68@gmail.com>
2019-01-08 12:43:56 +0330

    bug fix index 00

--- src/routes/board/operations.js
+++ src/routes/board/operations.js
@@ -1,77 +1,78 @@
-
-export function makeRowAndCol(num){
-  if(num <= 9){
-    num = 0 + '' + num.toString()
-  }
-  num = num.toString();
-  return {row: parseInt(num[0]), col: parseInt(num[1])}
-
-}
-export function parser(row, col){
-  if(!isNaN(row + '' + col) && parseInt(row + '' + col) > 0 && parseInt(row + '' + col) < 99){
-    return parseInt(row + '' + col)
-  }
-    return undefined
-  }
-
-export function generateGameCellsByLevel(level, rowInit, colInit){
-  let gameCells = [parseInt(rowInit + '' + colInit)];
-  let moveAreas = findMoveAreas(rowInit, colInit);
-  for(let i=0; i < level; i++){
-
-    let isAppended = false;
-    while(!isAppended){
-      const randomNumber = Math.floor(Math.random() * moveAreas.length) ;
-      if(!gameCells.includes(moveAreas[randomNumber])){
-        isAppended = true;
-        gameCells.push(moveAreas[randomNumber]);
-      }
-      if(isAppended){
-        let newPoint = makeRowAndCol(moveAreas[randomNumber])
-        moveAreas = findMoveAreas(newPoint.row, newPoint.col)
-  
-      }
+export function makeRowAndCol(num) {
+    if (num <= 9) {
+        num = 0 + '' + num.toString()
     }
-  }
-  return gameCells;
+    num = num.toString();
+    return {row: parseInt(num[0]), col: parseInt(num[1])}
+
 }
 
-export function findMoveAreas(row, col){
- let moveAreas = [];
-
- if(parser(row + 3, col)){
-   moveAreas.push(parser(row + 3, col));
- }
- if(parser(row - 3, col)){
-   moveAreas.push(parser(row - 3, col));
- }
- if(parser(row, col + 3)){
-   moveAreas.push(parser(row , col + 3));
- }
- if(parser(row, col - 3)){
-   moveAreas.push(parser(row , col - 3));
- }
- // 
- if(parser(row + 2, col - 2)){
-   moveAreas.push(parser(row +2  , col - 2));
- }
- if(parser(row + 2, col + 2)){
-   moveAreas.push(parser(row + 2  , col + 2));
- }
- if(parser(row - 2, col + 2)){
-   moveAreas.push(parser(row  - 2  , col + 2));
- }
- if(parser(row - 2, col - 2)){
-   moveAreas.push(parser(row - 2  , col - 2));
-
- }  return moveAreas
+export function parser(row, col) {
+    if (!isNaN(row + '' + col) && parseInt(row + '' + col) >= 0 && parseInt(row + '' + col) <= 99) {
+        return parseInt(row + '' + col)
+    }
+    return undefined
 }
 
-export function calculateLives(currentLives, unselectedItemsLength){
+export function generateGameCellsByLevel(level, rowInit, colInit) {
+    let gameCells = [parseInt(rowInit + '' + colInit)];
+    let moveAreas = findMoveAreas(rowInit, colInit);
+    for (let i = 0; i < level; i++) {
+
+        let isAppended = false;
+        while (!isAppended) {
+            const randomNumber = Math.floor(Math.random() * moveAreas.length);
+            if (!gameCells.includes(moveAreas[randomNumber])) {
+                isAppended = true;
+                gameCells.push(moveAreas[randomNumber]);
+            }
+            if (isAppended) {
+                let newPoint = makeRowAndCol(moveAreas[randomNumber])
+                moveAreas = findMoveAreas(newPoint.row, newPoint.col)
+
+            }
+        }
+    }
+    return gameCells;
+}
+
+export function findMoveAreas(row, col) {
+    let moveAreas = [];
+
+    if (parser(row + 3, col) !== undefined) {
+        moveAreas.push(parser(row + 3, col));
+    }
+    if (parser(row - 3, col) !== undefined) {
+        moveAreas.push(parser(row - 3, col));
+    }
+    if (parser(row, col + 3) !== undefined) {
+        moveAreas.push(parser(row, col + 3));
+    }
+    if (parser(row, col - 3) !== undefined) {
+        moveAreas.push(parser(row, col - 3));
+    }
+    //
+    if (parser(row + 2, col - 2) !== undefined) {
+        moveAreas.push(parser(row + 2, col - 2));
+    }
+    if (parser(row + 2, col + 2) !== undefined) {
+        moveAreas.push(parser(row + 2, col + 2));
+    }
+    if (parser(row - 2, col + 2) !== undefined) {
+        moveAreas.push(parser(row - 2, col + 2));
+    }
+    if (parser(row - 2, col - 2) !== undefined) {
+        moveAreas.push(parser(row - 2, col - 2));
+
+    }
+    return moveAreas
+}
+
+export function calculateLives(currentLives, unselectedItemsLength) {
     let newLives = 0;
 
-    if(unselectedItemsLength === 0){
-      newLives = currentLives + 1;
+    if (unselectedItemsLength === 0) {
+        newLives = currentLives + 1;
     } else {
         newLives = currentLives - 1;
     }
