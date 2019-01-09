728b0ba332bda3b3f8350f24f2e6b2d9673193c5
Amish <amir.shabanai68@gmail.com>
2019-01-09 11:27:57 +0330

    how to play text

--- src/routes/howPlay/index.android.js
+++ src/routes/howPlay/index.android.js
@@ -1,4 +1,4 @@
-import {View, Image} from 'react-native';
+import {View, Image, Text} from 'react-native';
 import React, {Component} from 'react';
 import Styles from "./styles.android";
 
@@ -13,6 +13,11 @@ export default class ConfigScreen extends Component<{}> {
                 <Image
                     source={require('../../assets/img/play.png')}
                 />
+                <View style={{backgroundColor: 'green', justifyContent: 'center', alignItems: 'center'}}>
+                    <Text>در هر مرحله فقط یک بار می توانید از دکمه بازگشت استفاده کنید.</Text>
+                    <Text>اگر بر روی نقطه سبز اشتباه کلیک کنید یک جوووون (به همین غلظت) از دست می دهید.</Text>
+                    <Text>اگر جووونت صفر بشه به نزدیک ترین مرحله مضرب ۳ سقوط می کنید.</Text>
+                </View>
             </View>
         )
     }
